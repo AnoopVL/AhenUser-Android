47a0fbbf339099b364f662308b30eacb6efb08a6
AnoopVL <avlanjekar4@gmail.com>
2024-03-08 23:34:49 +0530

    Map activity started

---
+++ app/src/main/java/com/avl/ahenuser/map.java
@@ -0,0 +1,14 @@
+package com.avl.ahenuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
