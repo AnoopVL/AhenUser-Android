c0350c696c32d7e408f1b3cb3add0871cdf0c231
AnoopVL <avlanjekar4@gmail.com>
2024-04-24 00:59:23 +0530

    Profile Fragment fixed

--- app/src/main/java/com/avl/ahenuser/ProfileFragment.java
+++ app/src/main/java/com/avl/ahenuser/ProfileFragment.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.os.Parcelable;
@@ -18,6 +19,11 @@ import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,15 +31,15 @@ import com.google.android.material.textfield.TextInputLayout;
  * create an instance of this fragment.
  */
 public class ProfileFragment extends Fragment {
-    private TextView fullNameTextView;
+    private TextView fullName_textView;
     private TextView phone_TextView;
     private TextInputLayout fullName_TextInputLayout;
     private TextInputLayout email_TextInputLayout;
     private TextInputLayout phone_TextInputLayout;
     private TextInputLayout password_TextInputLayout;
-    private TextInputEditText fullNameEditText;
-    private TextInputEditText emailEditText;
-    private TextInputEditText phoneEditText;
+    private TextInputEditText full_name_edittext;
+    private TextInputEditText email_edittext;
+    private TextInputEditText phone_edittext;
     private TextInputEditText passwordEditText;
     private Button updateButton;
     private Button signOut;
@@ -42,16 +48,7 @@ public class ProfileFragment extends Fragment {
     public ProfileFragment() {
         // Required empty public constructor
     }
-//    public static ProfileFragment newInstance(String fullName, String email, String phone, String password) {
-//        ProfileFragment fragment = new ProfileFragment();
-//        Bundle args = new Bundle();
-//        args.putString("fullName", fullName);
-//        args.putString("email", email);
-//        args.putString("phone", phone);
-//        args.putString("password", password);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
+
 public static ProfileFragment newInstance(UserHelper userData) {
     ProfileFragment fragment = new ProfileFragment();
     Bundle args = new Bundle();
@@ -69,19 +66,52 @@ public static ProfileFragment newInstance(UserHelper userData) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
+
+        fullName_textView = view.findViewById(R.id.fullName_textView);
         fullName_TextInputLayout = view.findViewById(R.id.fullName_TextInputLayout);
+        full_name_edittext = view.findViewById(R.id.full_name_edittext);
         email_TextInputLayout = view.findViewById(R.id.email_TextInputLayout);
+        email_edittext = view.findViewById(R.id.email_edittext);
         phone_TextInputLayout = view.findViewById(R.id.phone_TextInputLayout);
-        password_TextInputLayout = view.findViewById(R.id.password_TextInputLayout);
-        fullNameTextView = view.findViewById(R.id.fullName_textView);
-        phone_TextView = view.findViewById(R.id.phone_textView);
-        updateButton = view.findViewById(R.id.updateButton);
+        phone_edittext = view.findViewById(R.id.phone_edittext);
         signOut = view.findViewById(R.id.sign_out);
 
-        fullNameEditText = (TextInputEditText) fullName_TextInputLayout.getEditText();
-        emailEditText = (TextInputEditText) email_TextInputLayout.getEditText();
-        phoneEditText = (TextInputEditText) phone_TextInputLayout.getEditText();
-        passwordEditText = (TextInputEditText) password_TextInputLayout.getEditText();
+//        fullNameEditText = (TextInputEditText) fullName_TextInputLayout.getEditText();
+//        emailEditText = (TextInputEditText) email_TextInputLayout.getEditText();
+//        phoneEditText = (TextInputEditText) phone_TextInputLayout.getEditText();
+//        passwordEditText = (TextInputEditText) password_TextInputLayout.getEditText();
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("userLogin", MODE_PRIVATE);
+        String phoneNumber = sharedPreferences.getString("phoneNumber", "");
+
+        DatabaseReference userRef = FirebaseDatabase.getInstance().getReference().child("users").child(phoneNumber);
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Check if the user data exists
+                if (dataSnapshot.exists()) {
+                    // Get the user data
+                    String fullName = dataSnapshot.child("name").getValue(String.class);
+                    String email = dataSnapshot.child("email").getValue(String.class);
+                    String phone = dataSnapshot.child("phone").getValue(String.class);
+
+                    // Set the retrieved data to the corresponding views
+                    fullName_textView.setText(fullName);
+                    full_name_edittext.setText(fullName);
+                    email_edittext.setText(email);
+                    phone_edittext.setText(phone);
+                } else {
+                    // Handle the case where the user data does not exist
+                    Toast.makeText(getActivity(), "User data not found", Toast.LENGTH_SHORT).show();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
 
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -97,27 +127,6 @@ public static ProfileFragment newInstance(UserHelper userData) {
             }
         });
 
-        // Extracting data from arguments
-        Bundle args = getArguments();
-        if (args != null) {
-            String fullName = args.getString("fullName");
-            String email = args.getString("email");
-            String phone = args.getString("phone");
-            String password = args.getString("password");
-
-            fullNameTextView.setText(fullName);
-            fullNameEditText.setText(fullName);
-            emailEditText.setText(email);
-            phoneEditText.setText(phone);
-            phone_TextView.setText(phone);
-            passwordEditText.setText(password);
-        }
-        updateButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Sorry, try again later!!", Toast.LENGTH_SHORT).show();
-            }
-        });
         return view;
     }
 }
